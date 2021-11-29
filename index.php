327f656622173b056499f21b92b92973477c8110
Softdesk Media <softdeskmedia@gmail.com>
2021-11-30 00:47:53 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <title>Landing Page</title>
+      <meta name="description" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+      <link rel="stylesheet" href="assets/css/style.css" />
+      <link rel="stylesheet" href="assets/css/style2.css">
+   </head>
+   <body>
+      <main>
+         <div class="slider-area slider-bg1">
+            <div class="slider-active">
+               <div class="single-slider d-flex align-items-center slider-height ">
+                  <div class="container">
+                     <div class="row align-items-center justify-content-between" style="margin-top: -200px;">
+                        <div class="col-xl-6 col-lg-6 col-md-7 ">
+                           <div class="hero__caption">
+                              <h1 data-animation="fadeInLeft" data-delay=".6s" style="font-size: 35px; ">Full House Floor Plan | Interior Design | 3D Architectural Designing Services</h1>
+                              <p data-animation="fadeInLeft" data-delay=".8s">We offer professional architectural design services as per the latest trends and with assurance of best space management and positive energy.
+                                 <br />
+                                 We specialize in delivering Architectural 3D modeling, design, and visualization support for new built, retrofits, refurbishment and renovation projects. We possess an extensive range of skills and are fully committed to design excellence.
+                              </p>
+                           </div>
+                        </div>
+                        <div class="col-xl-4 p-0 col-lg-5 col-md-5">
+                           <div class="form-wrapper" data-animation="bounceIn" data-delay="0.2s" style="margin: 10px;">
+                              <div class="form-tittle mb-10 text-center">
+                                 <h2>Get a Free Quote for your Dream Home</h2>
+                                 <p>Connect our expert</p>
+                              </div>
+                              <form id="contact-form" action="#" method="POST">
+                                 <div class="col-lg-12">
+                                    <div class="form-box mb-15">
+                                       <input type="text" name="name" placeholder="Name">
+                                    </div>
+                                 </div>
+                                 <div class="col-lg-12">
+                                    <div class="form-box mb-15">
+                                       <input type="text" name="number" placeholder="Phone no.">
+                                    </div>
+                                 </div>
+                                 <div class="col-lg-12">
+                                    <div class="form-box mb-15">
+                                       <input type="text" name="text" placeholder="City">
+                                    </div>
+                                 </div>
+                                 <div class="col-lg-12">
+                                    <div class="form-box mb-15">
+                                       <input type="text" name="text" placeholder="Email">
+                                    </div>
+                                 </div>
+                                 <div class="submit-info">
+                                    <button class="submit-btn2" type="submit">Continue</button>
+                                 </div>
+                           </div>
+                        </div>
+                        </form>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+         </div>
+         <section class="services-section gray-bg section-padding">
+            <div class="container">
+               <div class="row justify-content-center">
+                  <div class="col-xl-5 col-lg-11 ">
+                     <div class="section-tittle  text-center mb-75">
+                        <h2>Services</h2>
+                     </div>
+                  </div>
+               </div>
+               <div class="services-active">
+                  <div class="row">
+                     <div class="col-lg-4 col-md-4 col-sm-6">
+                        <div class="single-cat mb-30">
+                           <div class="cat-img">
+                              <img src="assets/img/gallery/Elevation.jpeg" alt="" data-pagespeed-url-hash="2609175158" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+                           </div>
+                           <div class="cat-cap">
+                              <h5><a href="https://archdesignworld.com/house-elevation">Elevation Design</a></h5>
+                              <p>To fulfil the diverse requirements of our prestigious customers, we are highly intricate in providing Home Elevation Design Service.</p>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="col-lg-4 col-md-4 col-sm-6">
+                        <div class="single-cat mb-30">
+                           <div class="cat-img">
+                              <img src="assets/img/gallery/Floorplan.png" alt="" data-pagespeed-url-hash="2903675079" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+                           </div>
+                           <div class="cat-cap">
+                              <h5><a href="https://archdesignworld.com/floor-plan">Floor Plan</a></h5>
+                              <p>We help you get your concerns out of the way because we are a leading 2D/3D floor plan creation service provider having the right bandwidth and technology.</p>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="col-lg-4 col-md-4 col-sm-6">
+                        <div class="single-cat mb-30">
+                           <div class="cat-img">
+                              <img src="assets/img/gallery/Interior.jpeg" alt="" data-pagespeed-url-hash="2903675079" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+                           </div>
+                           <div class="cat-cap">
+                              <h5><a href="https://archdesignworld.com/interior-design">Interior Plan</a></h5>
+                              <p>Get beautiful home interior design services with Complete home interiors solutions at lowest price and best quality. Decorist online interior design helps you transform any room in your home.</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </section>
+      </main>
+   </body>
+</html>
